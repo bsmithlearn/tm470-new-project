4000e971aee2151e15453837c296a1ced7d94076
ben smith <bensturf@aol.com>
2024-08-16 15:51:37 +0100

    cautions edit

--- ccautions/src/App.js
+++ ccautions/src/App.js
@@ -1,16 +1,16 @@
 //App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AddressList from './components/AddressList';
-import AddAddress from './components/AddAddress';
+import CautionList from './components/CautionList';
+import AddCaution from './components/AddCaution';
 
 function App() {
 return (
     <Router>
     <div className='container container-fluid min-vh-100 d-flex flex-column'>
         <Routes>
-        <Route exact path="/" element={<AddressList/>} component={AddressList} />
-        <Route exact path="/add" element={<AddAddress/>} component={AddAddress} />
+        <Route exact path="/" element={<CautionList/>} component={CautionList} />
+        <Route exact path="/add" element={<AddCaution/>} component={AddCaution} />
         </Routes>
     </div>
     </Router>
