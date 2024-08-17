4028ff22701f61a9e8e2c703562f9c32927438e4
ben smith <bensturf@aol.com>
2024-08-17 16:23:39 +0100

    updated schema for cautions

--- caution-backend/models/caution.js
+++ caution-backend/models/caution.js
@@ -3,10 +3,35 @@ const mongoose = require('mongoose');
 
 //caution schema for storing cautions
 const cautionSchema = new mongoose.Schema({
-name: String,
+
+// defendant info
+forename: String,
+surname: String,
+dob: Date,
+addLineOne: String,
+addLineTwo: String,
+addLineThree: String,
+postcode: String,
+homePhone: Number,
+mobilePhone: Number,
 email: String,
-contact: String,
-address : String
+
+// police officer info
+officerID: String,
+officerSurname: String,
+officerRank: String,
+forceID: Number,
+
+// offence info
+offenceDesc: String,
+offenceDate: Date,
+offenceLocation: String,
+
+// status
+imposedAmt: mongoose.Types.Decimal128,
+paidAmt: mongoose.Types.Decimal128,
+balance: mongoose.Types.Decimal128,
+acctStatus: String
 });
 
 module.exports = mongoose.model('Caution', cautionSchema , 'cautions');
