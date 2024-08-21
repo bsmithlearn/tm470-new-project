c0abdd071e54606ff071033c626a931f53257def
ben smith <bensturf@aol.com>
2024-08-21 16:44:07 +0100

    removed live search filter

--- ccautions/src/App.js
+++ ccautions/src/App.js
@@ -3,13 +3,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CautionList from './components/CautionList';
 import AddCaution from './components/AddCaution';
-import LiveSearchFilter from './components/LiveSearchFilter.js'
 
 function App() {
 return (
     <Router>
     <div className='container container-fluid min-vh-100 d-flex flex-column'>
-        <LiveSearchFilter/>
         <Routes>
         <Route exact path="/" element={<CautionList/>} component={CautionList} />
         <Route exact path="/add" element={<AddCaution/>} component={AddCaution} />
